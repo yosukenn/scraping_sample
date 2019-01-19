9e2b31b348239c3b8599449ba0b8c1369c7ba2a8
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2019-01-19 16:00:32 +0900

    add Exception handling

--- src/main/java/com/demo/api/ArticleRestController.java
+++ src/main/java/com/demo/api/ArticleRestController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,26 @@ public class ArticleRestController {
 	@Autowired
 	ArticleService articleService;
 	
+	/**
+	 * 記事を取得するリクエストへのマッピング
+	 * @param url 取得したいニュースのURL
+	 * @return 取得した記事情報（Asrticle型）
+	 * @throws IOException
+	 */
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	Article postArticle(@RequestBody String url) throws IOException {
 		return articleService.createArticle(url);
 	}
+	
+	/**
+	 * IOException の例外ハンドラー
+	 * @param e エラーオブジェクト
+	 * @return エラー時のJSONメッセージ
+	 */
+	@ExceptionHandler(IOException.class)
+	public ResponseEntity<String> handleIOException(IOException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+	
 }
